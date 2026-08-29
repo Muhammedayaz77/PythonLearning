d6b4ccb5cbc941107a99c58d0c478739e2910314
Mohammed Ayaz <ayaz.job2010@gmail.com>
2026-08-29 15:42:48 +0530

    Add variables and data types example

---
+++ variables_and_data_types.py
@@ -0,0 +1,29 @@
+# Store values in variables.
+studentNameStr = "Ayaz"
+studentIdInt = 7
+studentMarksFloat = 450.5
+isActiveBool = True
+
+# Check the data type of a value.
+print("Name:", studentNameStr)
+print("Name type:", type(studentNameStr))
+print("ID type:", type(studentIdInt))
+print("Marks type:", type(studentMarksFloat))
+print("Active type:", type(isActiveBool))
+
+# A variable can be changed to another value.
+studentMarksFloat = 480.5
+print("Updated marks:", studentMarksFloat)
+
+# Convert values between common data types.
+ageStr = "30"
+ageInt = int(ageStr)
+print("Age:", ageInt)
+
+priceInt = 100
+priceFloat = float(priceInt)
+print("Price:", priceFloat)
+
+numberInt = 25
+numberStr = str(numberInt)
+print("Number as text:", numberStr)
